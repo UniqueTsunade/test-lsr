23273b53044951da7ad7d9de5c0fd6411c514a0b
Kate <popovaekaterina13@yandex.ru>
2025-04-19 18:02:24 +0300

    style: styling of the ApartmentCard component has been done

--- src/components/ApartmentCard/ApartmentCard.tsx
+++ src/components/ApartmentCard/ApartmentCard.tsx
@@ -1,42 +1,69 @@
 import ApartmentPlan from '../../assets/plan.png'
+import './apartmentCard.css'
 
 const ApartmentCard = () => {
   return (
-    <div>
-      <a href="#!">
-        <div>
-          <span>дом 43 эт. 3/32</span>
-          <span>2-комнатная евро квартира</span>
+    <div className="apartment-card">
+      <a
+        href="#!"
+        className="apartment-card__link"
+        aria-label="Подробнее о квартире, 2-комнатная евро, 14 598 252 ₽"
+      >
+        <div className="apartment-card__label">
+          <span className="apartment-card__building">дом 43</span>
+          <span className="apartment-card__floor">эт. 3/32</span>
         </div>
-        <div>
-          <img src={ApartmentPlan} alt="2-комнатная евро квартира" />
+
+        <div className="apartment-card__header">
+          <span className="apartment-card__type">
+            2-комнатная евро квартира
+          </span>
         </div>
-        <div>
-          <span>14 598 252 ₽</span>
+
+        <div className="apartment-card__image">
+          <img
+            src={ApartmentPlan}
+            alt="2-комнатная евро квартира"
+            className="apartment-card__plan"
+          />
+        </div>
+
+        <div className="apartment-card__details">
+          <span className="apartment-card__price">14 598 252 ₽</span>
         </div>
       </a>
-      <div>
-        <a href="#!">
-          <span>Подробнее</span>
+
+      <div className="apartment-card__footer">
+        <a
+          href="#!"
+          className="apartment-card__more"
+          aria-label="Посмотреть подробнее"
+        >
+          <span className="apartment-card__more-text">Подробнее</span>
         </a>
       </div>
-      <div>
-        <button>
+
+      <div className="apartment-card__favourite">
+        <button
+          type="button"
+          className="apartment-card__favourite-btn apartment-card__favourite-btn--is-active"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="200"
-            height="200"
+            width="24"
+            height="24"
             viewBox="0 0 24 24"
+            className="apartment-card__favourite-icon"
+            aria-label="Добавить в избранное"
           >
             <path
               d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5
-    2 5.42 4.42 3 7.5 3c1.74 0 3.41 0.81 4.5 2.09
-    C13.09 3.81 14.76 3 16.5 3
-    19.58 3 22 5.42 22 8.5
-    c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+                   2 5.42 4.42 3 7.5 3c1.74 0 3.41 0.81 4.5 2.09
+                   C13.09 3.81 14.76 3 16.5 3
+                   19.58 3 22 5.42 22 8.5
+                   c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
               fill="none"
-              stroke="grey"
-              strokeWidth="0.3"
+              strokeWidth="1"
             />
           </svg>
         </button>
