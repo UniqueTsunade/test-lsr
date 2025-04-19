d55eaa0465256b29f6eea37f26977b5ce75969d9
Kate <popovaekaterina13@yandex.ru>
2025-04-19 15:44:56 +0300

    feat: ApartmentCard component created

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import ApartmentCard from '../components/ApartmentCard'
+import './app.css'
+
+function App() {
+  return (
+    <div className="container">
+      <ApartmentCard />
+    </div>
+  )
+}
+
+export default App
